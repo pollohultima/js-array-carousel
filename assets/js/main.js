84cd11fc02f2486f238fa2757e4f1127dd1034bc
pollohultima <matteo.polonio@gmail.com>
2021-10-15 13:01:32 +0200

    Created static version
    buttons still missing

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,5 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+
